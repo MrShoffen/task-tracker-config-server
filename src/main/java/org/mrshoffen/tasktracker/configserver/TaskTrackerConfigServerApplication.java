1a49dfaaf8dacca9a2a1385cf0a8c7eb11e0c09f
andrey_vasiliev <mrshoffen@gmail.com>
2025-04-19 00:20:41 +0300

    initial commit

---
+++ src/main/java/org/mrshoffen/tasktracker/configserver/TaskTrackerConfigServerApplication.java
@@ -0,0 +1,15 @@
+package org.mrshoffen.tasktracker.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class TaskTrackerConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerConfigServerApplication.class, args);
+    }
+
+}
