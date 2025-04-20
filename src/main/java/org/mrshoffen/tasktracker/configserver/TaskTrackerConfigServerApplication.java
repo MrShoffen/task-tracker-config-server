122f62baa58d9ed6f744a4c3a82133d6c935db64
andrey_vasiliev <mrshoffen@gmail.com>
2025-04-20 20:20:51 +0300

    feat: use old variant of configure - bootstrap.yml for one purpose - to allow config server fetch configuration for itself.

--- src/main/java/org/mrshoffen/tasktracker/configserver/TaskTrackerConfigServerApplication.java
+++ src/main/java/org/mrshoffen/tasktracker/configserver/TaskTrackerConfigServerApplication.java
@@ -3,6 +3,8 @@ package org.mrshoffen.tasktracker.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableConfigServer
