4c8179b13f134012e786fb289d34d4d777ea3780
andrey_vasiliev <mrshoffen@gmail.com>
2025-04-20 17:37:16 +0300

    feat: add actuator health endpoint

--- src/main/java/org/mrshoffen/tasktracker/configserver/security/SecurityConfig.java
+++ src/main/java/org/mrshoffen/tasktracker/configserver/security/SecurityConfig.java
@@ -3,10 +3,13 @@ package org.mrshoffen.tasktracker.configserver.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.lang.reflect.Method;
 
 @Configuration
 public class SecurityConfig {
@@ -14,11 +17,14 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return
-                http.csrf(AbstractHttpConfigurer::disable)
-                        .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                        .httpBasic(Customizer.withDefaults())
-                        .build();
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/actuator/health/**").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
     }
 
 }
